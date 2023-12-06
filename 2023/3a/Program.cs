3d5b7cf0d2cc78d14cd9774f7fe682075c12e509
Matt Little <matthewtlittle@gmail.com>
2023-12-06 10:48:48 +1000

    finish day 3

--- 2023/3a/Program.cs
+++ 2023/3a/Program.cs
@@ -6,9 +6,8 @@ using System.Diagnostics.CodeAnalysis;
 
 Regex rx = new Regex(@"\d+");
 
-var lines = File.ReadLines("input.txt", Encoding.UTF8);
+var lines = File.ReadLines("../../../input.txt", Encoding.UTF8);
 var linesList = new List<string>(lines);
-var LIMIT = linesList.Count-1;
 char[] symbols = {
     '#',
     '%',
@@ -23,46 +22,29 @@ char[] symbols = {
 };
 var sum = 0;
 
-// NEED TO CONSIDER EDGE CASES
-// FIRST LINE
-// LAST LINE
-// FAR LEFT OR FAR RIGHT NUMBERS
+string dots = "..............................................................................................................................................";
+linesList.Insert(0, dots);
+linesList.Insert(linesList.Count, dots);
 
-// First line
-MatchCollection matches = rx.Matches(linesList[0]);
-matches = rx.Matches(linesList[0]);
-    foreach(Match match in matches) {
-        var start = match.Index-1;
-        var length = match.Length + 2;
-        // how do we quickly iterate .Contains over a list?
-        for(int j = 0; j <= 1; j++){
-            if(linesList[0+j].Substring(start, length).IndexOfAny(symbols) > -1)
-            {
-                // convert match to int
-                // add to sum
-                sum += int.Parse(match.Value);   
-            }
-        }
-    }
-// Last Line
 
-for(int i = 1; i < LIMIT; i++) 
+
+var LIMIT = linesList.Count-1;
+for(int i = 0; i < LIMIT; i++) 
 {
-    matches = rx.Matches(linesList[i]);
+    linesList[i+1] = linesList[i+1].Insert(0, ".");
+    linesList[i+1] = linesList[i+1].Insert(linesList[i+1].Length, ".");
+    MatchCollection matches = rx.Matches(linesList[i]);
     foreach(Match match in matches) {
         var start = match.Index-1;
         var length = match.Length + 2;
-        // how do we quickly iterate .Contains over a list?
         for(int j = -1; j <= 1; j++){
             if(linesList[i+j].Substring(start, length).IndexOfAny(symbols) > -1)
             {
-                // convert match to int
-                // add to sum
-                sum += int.Parse(match.Value);   
+                sum += int.Parse(match.Value);
             }
         }
     }
 }
 
-Console.WriteLine(sum);
+Console.WriteLine("Total Sum = {0}", sum);
 
