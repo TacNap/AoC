d9e31c34c3c517a64d7f5a13d4279f553283620b
Matthew <matthewtlittle@gmail.com>
2026-01-06 19:12:20 +1000

    completed Day 7

---
+++ 2025_b/Day7/Day7.App/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int splitCount = 0;
+        string[] tree = File.ReadAllLines("input.txt");
+        List<bool> beams = new List<bool>(141);
+        for (int i = 0; i < 141; i++)
+        {
+            beams.Add(false);
+        }
+        beams[70] = true; // Set S - initial starting beam
+        foreach (var line in tree)
+        {
+            int lineCount = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '^' && beams[i] == true)
+                {
+                    beams[i] = false;
+                    beams[i - 1] = true;
+                    beams[i + 1] = true;
+                    splitCount++;
+                    lineCount++;
+                }
+            }
+            Console.WriteLine($"Line Count: {lineCount}");
+        }
+        Console.WriteLine(splitCount);
+    }
+}
